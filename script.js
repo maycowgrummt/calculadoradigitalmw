2d6df2565a41f4b8d950379e57c7ecccf8112d3b
maycowgrummt <grummtmaycow@hotmail.com>
2025-07-25 15:37:43 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,32 @@
 let display = document.getElementById("display");
 
 function adicionar(valor) {
+  display.classList.remove("resultado");
   display.value += valor;
 }
 
 function limpar() {
+  display.classList.remove("resultado");
   display.value = "";
 }
 
 function apagar() {
+  display.classList.remove("resultado");
   display.value = display.value.slice(0, -1);
 }
 
 function calcular() {
   try {
-    display.value = eval(display.value.replace("%", "/100"));
+    const resultado = eval(display.value.replace("%", "/100"));
+    display.value = resultado;
+    display.classList.add("resultado");
   } catch {
     display.value = "Erro";
+    display.classList.remove("resultado");
   }
 }
 
-// Habilita o teclado físico
+// Atalhos do teclado físico
 document.addEventListener("keydown", function(e) {
   const tecla = e.key;
 
